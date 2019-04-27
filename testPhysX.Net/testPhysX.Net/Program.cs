5ca3104e662975114eadc3a7610952a9980abcc4
wth <690879430@qq.com>
2019-04-27 15:51:31 +0800

    测试physx.net

---
+++ testPhysX.Net/testPhysX.Net/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhysX;
+using System.Numerics;
+//namespace testPhysX.Net
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Foundation foundation = new Foundation();
+        Physics physics = new Physics(foundation);
+        Scene scene = physics.CreateScene();
+
+        Matrix4x4 mat = Matrix4x4.CreateTranslation(new Vector3(0, 0, 2));
+        var actor = scene.Physics.CreateRigidDynamic(mat);
+
+        Material material = scene.Physics.CreateMaterial(0.7f, 0.7f, 0.1f);
+
+        SphereGeometry sphereGeo = new SphereGeometry(1);
+        actor.CreateShape(sphereGeo, material);
+        scene.AddActor(actor);
+
+        HitFlag flag = HitFlag.Default | HitFlag.Normal;
+        bool ret = scene.Raycast(Vector3.Zero, new Vector3(0, 0, 1), 100, 100, HitCallback, flag);
+
+        Console.WriteLine(ret);
+
+        Console.WriteLine("运行结束");
+        Console.ReadLine();
+    }
+
+    static bool HitCallback(RaycastHit[] array)
+    {
+        //Console.WriteLine("HitCallback" + flag);
+        Console.WriteLine("HitCallback " + array.Length);
+        return true;
+    }
+}
+
